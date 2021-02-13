274d2999f97208dda4b1bed732c9df718f7d7364
Lucas Ribeiro <zero@bbl.gg>
2021-02-13 11:24:51 -0300

    ok

--- backend/src/controllers/FetchController.js
+++ backend/src/controllers/FetchController.js
@@ -1,27 +1,25 @@
-const ogs = require('open-graph-scraper')
-
 module.exports = {
     async fetch(req, res) {
         const options = { url: req.query.url }
 
-        ogs(options, (error, results, response) => {
-            console.log(results)
-            if (results.success) {
-                return res.json({
-                    "success" : 1,
-                    "meta": {
-                        "title" : results.ogTitle,
-                        "description" : results.ogDescription,
-                        "image" : {
-                            "url" : results.ogImage.url
-                        }
-                    }
-                })
-            } else {
-                return res.json({
-                    success: 0
-                })
-            }
-        });
+        // ogs(options, (error, results, response) => {
+        //     console.log(results)
+        //     if (results.success) {
+        //         return res.json({
+        //             "success" : 1,
+        //             "meta": {
+        //                 "title" : results.ogTitle,
+        //                 "description" : results.ogDescription,
+        //                 "image" : {
+        //                     "url" : results.ogImage.url
+        //                 }
+        //             }
+        //         })
+        //     } else {
+        //         return res.json({
+        //             success: 0
+        //         })
+        //     }
+        // });
     },
 }
--- backend/src/controllers/UploadController.js
+++ backend/src/controllers/UploadController.js
@@ -1,3 +1,8 @@
+const crypto = require("crypto")
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+
 module.exports = {
     async image(req, res) {
         if (req.file) {
@@ -14,17 +19,20 @@ module.exports = {
         }
     },
     async imageByURL(req, res) {
-        if (req.file) {
-            return res.json({
-                success: 1,
+        const { url } = req.body
+        const extension = url.split('.').pop()
+        crypto.randomBytes(16, (err, hash) => {
+            let filename = `${hash.toString("hex")}.${extension}`;
+            const file = fs.createWriteStream(path.join(__dirname, "..", "images", "posts", filename))
+            https.get(url, function(response) {
+                response.pipe(file);
+                return res.json({
+                    success: 1,
                     file: {
-                    url: `${process.env.API_IMAGE}${req.file.filename}`
-                }
+                        url: `${process.env.API_IMAGE}${filename}`
+                    }
+                })
             })
-        } else {
-            return res.json({
-                success: 0
-            })
-        }
+        })
     }
 }
